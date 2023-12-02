58710d10bd238a2a60cdd977fa126021f9ea363e
Minh61523 <minhltgcs210913@fpt.edu.vn>
2023-12-02 21:33:10 +0700

    MWS Project

---
+++ js/database.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB5mFPtYnaG6rfLNT6gEed9i8NZLR48LaE",
+    authDomain: "mws-project-6c394.firebaseapp.com",
+    projectId: "mws-project-6c394",
+    storageBucket: "mws-project-6c394.appspot.com",
+    messagingSenderId: "199591138506",
+    appId: "1:199591138506:web:6facfc65562c9a85625033"
+  };
